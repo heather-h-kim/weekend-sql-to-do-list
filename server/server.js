3d108a8747e05f8e40a4e40255fc6f51b7b18bb4
Heather Kim <heatherkim0405@gmail.com>
2022-02-14 15:45:28 -0600

    set up PORT for heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('server/public'));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 let router = require('./routes/todo.router');
 
